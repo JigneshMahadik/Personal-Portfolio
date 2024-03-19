ee6fd128e2ef7142e8b3d4033fb27eaa766c96e1
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2024-03-19 20:05:11 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -206,6 +206,48 @@ export function About(){
                     </div>
                 </div>
             </section>
+            {/* Favorite Activites */}
+            <section class="text-gray-400 bg-gray-900 body-font">
+                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Favorite Activities</h1>
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-800 sm:flex-row flex-col">
+                    <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full text-indigo-400 bg-gray-800 flex-shrink-0">
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
+                        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                        </svg>
+                    </div>
+                    <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+                        <h2 class="text-white text-lg title-font font-medium mb-2">Investments</h2>
+                        <p class="leading-relaxed text-base">Exploring Stocks, Crypto and Long-Term Growth Opportunities for Financial Investment</p>
+                    </div>
+                    </div>
+                    <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-800 sm:flex-row flex-col">
+                    <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+                        <h2 class="text-white text-lg title-font font-medium mb-2">Music</h2>
+                        <p class="leading-relaxed text-base">Creating Instrumentals, Mixing and Mastering Tracks and Collaborating with Musicians</p>
+                    </div>
+                    <div class="sm:w-32 order-first sm:order-none sm:h-32 h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full text-indigo-400 bg-gray-800 flex-shrink-0">
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
+                        <circle cx="6" cy="6" r="3"></circle>
+                        <circle cx="6" cy="18" r="3"></circle>
+                        <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                        </svg>
+                    </div>
+                    </div>
+                    <div class="flex items-center lg:w-3/5 mx-auto sm:flex-row flex-col">
+                    <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full text-indigo-400 bg-gray-800 flex-shrink-0">
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
+                        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                        <circle cx="12" cy="7" r="4"></circle>
+                        </svg>
+                    </div>
+                    <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+                        <h2 class="text-white text-lg title-font font-medium mb-2">Reading</h2>
+                        <p class="leading-relaxed text-base">Books on Self-Improvement, Psychology and Understanding Life Better.</p>
+                    </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
