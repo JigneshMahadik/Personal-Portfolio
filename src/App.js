e7e6dbc51c4686739d9a80fd00a38213323960a4
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2024-03-21 15:59:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import { Route,Routes } from "react-router-dom";
 import Certificates from './components/certificates';
 import { Projects } from './components/projects';
 import { About } from './components/about';
-import { More } from './components/more';
+import { Experience } from './components/experience';
 import { Contact } from './components/contact';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           <Route path="/"  element={<About/>} />
           <Route path="/Certificates"  element={<Certificates/>} />
           <Route path="/Projects"  element={<Projects/>} />
-          <Route path="/More"  element={<More/>} />
+          <Route path="/Experience"  element={<Experience/>} />
           <Route path="/Contact"  element={<Contact/>} />
         </Routes>
     </div>
